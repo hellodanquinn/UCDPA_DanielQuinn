2d080f22f9f920db941494451a6a80d96543f735
hellodanquinn <dannyquinn831@gmail.com>
2021-07-09 17:12:16 +0100

    Initial creation of this project with some simple test code in place to test project setup

---
+++ EaseOfBusiness.py
@@ -0,0 +1,20 @@
+
+# Installed 'country-convert' as will need this after import of CSV to convert country names to Alpha2
+# Installed Pandas
+# Numpy running 1.21.0
+# Import CSV file
+# Analyse data imported
+# Clean up data
+
+import pandas as pd
+print(pd.__version__)
+
+import numpy as np
+print(np.__version__)
+l = [1, 2, 3]
+l_array = np.array(l)
+print(type(l_array))
+
+hdg = "128"
+print(hdg + " Success")
+
