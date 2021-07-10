10fc11be4f8628bd46c197165e026bc58ca438e6
hellodanquinn <dannyquinn831@gmail.com>
2021-07-10 18:04:37 +0100

    Some updates after further testing

--- EaseOfBusiness.py
+++ EaseOfBusiness.py
@@ -29,7 +29,20 @@ data = pd.read_csv("Rankings_EaseOf.csv")
 # Preview data from the first 5 lines
 print(data.head())
 
-#function to convert to alpha3 country codes and continents
-country_code = pc.country_name_to_country_alpha3("China", cn_name_format="default")
-print(country_code)
+# function to convert to alpha3 country codes and continents
+# country_code = pc.country_name_to_country_alpha3("China", cn_name_format="default")
+# print(country_code)
+
+
+def country_to_continent(country_name):
+    country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+    country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+    country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+    return country_continent_name
+
+# Iterate through this on col2?
+# Example
+country_name = 'Germany'
+print((country_name) + " is a country in " + country_to_continent(country_name))
+
 
