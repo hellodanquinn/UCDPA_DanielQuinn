d1fec4edc92e4807f54fea2df809e056f7f4b0ce
hellodanquinn <dannyquinn831@gmail.com>
2021-07-12 15:40:36 +0100

    adding some further changes after testing data

--- EaseOfBusiness.py
+++ EaseOfBusiness.py
@@ -29,10 +29,13 @@ data = pd.read_csv("Rankings_EaseOf.csv")
 # Preview data from the first 5 lines
 print(data.head())
 
+# iterating the columns
+for col in data.columns:
+    print(col)
+
 # function to convert to alpha3 country codes and continents
 # country_code = pc.country_name_to_country_alpha3("China", cn_name_format="default")
 # print(country_code)
-
 
 def country_to_continent(country_name):
     country_alpha2 = pc.country_name_to_country_alpha2(country_name)
@@ -40,9 +43,16 @@ def country_to_continent(country_name):
     country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
     return country_continent_name
 
-# Iterate through this on col2?
-# Example
-country_name = 'Germany'
+# Test to validate
+country_name = 'Brazil'
 print((country_name) + " is a country in " + country_to_continent(country_name))
 
+def country_to_alpha(country_name):
+    country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+    country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+    country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+    return country_alpha2
 
+# Test to validate
+country_name = 'Ireland'
+print((country_name) + " has Alpha2 " + country_to_alpha(country_name))
